e9c7237ead592d2ef5d3325f3ff957b5c6bcbe65
Sveltecult <sveltecult@proton.me>
2024-07-28 10:01:20 +0800

    add blogs

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,4 @@
-import { z, defineCollection } from "astro:content";
+import { z, defineCollection, reference } from "astro:content";
 
 const docs = defineCollection({
   type: "content",
@@ -12,6 +12,38 @@ const docs = defineCollection({
   }),
 });
 
+const authors = defineCollection({
+  type: "data",
+  schema: z.object({
+    name: z.string(),
+    avatar: z.string(),
+  }),
+});
+
+const categories = defineCollection({
+  type: "data",
+  schema: z.object({
+    name: z.string(),
+  }),
+});
+
+const blogs = defineCollection({
+  type: "content",
+  schema: z.object({
+    authors: z.array(reference("authors")),
+    title: z.string(),
+    meta: z.optional(z.record(z.string())),
+    categories: z.array(reference("categories")),
+    excerpt: z.string().optional(),
+    image: z.string().optional(),
+    createdAt: z.date().transform((v) => new Date(v)),
+    updatedAt: z.date().transform((v) => new Date(v)),
+  }),
+});
+
 export const collections = {
   docs: docs,
+  authors: authors,
+  categories: categories,
+  blogs: blogs,
 };
